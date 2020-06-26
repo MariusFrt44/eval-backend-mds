24605cd28fe5f6c11edd271342d4dcc2bc3477fe
MariusFrt44 <62375125+MariusFrt44@users.noreply.github.com>
2020-06-26 10:49:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,79 @@
 <?php
+  /*****************************************
+  *  Constantes et variables
+  *****************************************/
+  define('Prenom','Rasmus');  // Login correct
+  define('PASSWORD','lerdorf');  // Mot de passe correct
+  $message = '';      // Message à afficher à l'utilisateur
+ 
+  /*****************************************
+  *  Vérification du formulaire
+  *****************************************/
+  // Si le tableau $_POST existe alors le formulaire a été envoyé
+  if(!empty($_POST))
+  {
+    // Le login est-il rempli ?
+    if(empty($_POST['login']))
+    {
+      $message = 'Veuillez indiquer votre login svp !';
+    }
+      // Le mot de passe est-il rempli ?
+      elseif(empty($_POST['motDePasse']))
+    {
+      $message = 'Veuillez indiquer votre mot de passe svp !';
+    }
+      // Le login est-il correct ?
+      elseif($_POST['login'] !== LOGIN)
+    {
+      $message = 'Votre login est faux !';
+    }
+      // Le mot de passe est-il correct ?
+      elseif($_POST['motDePasse'] !== PASSWORD)
+    {
+      $message = 'Votre mot de passe est faux !';
+    }
+      else
+    {
+      // L'identification a réussi
+      $message = 'Bienvenue '. LOGIN .' !';
+    }
+  }
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+  <head>
+    <title>Formulaire de base</title>
+  </head>
+  <body>
+    <?php if(!empty($message)) : ?>
+      <p><?php echo $message; ?></p>
+    <?php endif; ?>
+    <form action="<?php echo htmlspecialchars($_SERVER['REQUEST_URI'], ENT_QUOTES); ?>" method="post">
+      <fieldset>
+        <legend>Formulaire de base</legend>
+          <p>
+             <label for="Prénom">Prénom :</label> 
+            <input type="text" name="Prénom" id="Prénom" value="<?php if(!empty($_GET['Prénom'])) { echo htmlspecialchars($_GET['Prénom'], ENT_QUOTES); } ?>" />
+          </p>
+          <p>
+            <label for="Nom">Nom :</label> 
+            <input type="Nom" name="Nom" id="Nom" value="" /> 
 
-echo 'coucou1';
+          </p>
+                    <p>
+            <label for="Ville">Ville :</label> 
+            <input type="Ville" name="Ville" id="Ville" value="" /> 
+          </p>
+                              <p>
+            <label for="Mail">Mail :</label> 
+            <input type="Mail" name="Mail" id="Mail" value="" /> 
+          </p>
+                              <p>
+            <label for="phone">téléphone :</label> 
+            <input type="phone" name="phone" id="phone" value="" /> 
+            <input type="submit" name="submit" value="Envoyer" />
+          </p>
+      </fieldset>
+    </form>
+  </body>
+</html>
